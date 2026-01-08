527595b0f872ce841c4420f7df3244bbdc57e704
suke358 <123350462+suke358@users.noreply.github.com>
2026-01-08 16:59:21 +0900

    .入れる

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,13 @@
-const CACHE_NAME = 'meal-app-v1';
+const CACHE_NAME = 'meal-app-v2'; // v1からv2に変えると更新がスムーズです
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/script.js',
-  '/manifest.json'
+  './',
+  './index.html',
+  './css/style.css',
+  './js/script.js',
+  './manifest.json'
 ];
 
+    // ...以下のコードは変更なし
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
