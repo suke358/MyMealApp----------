009b4cac5cff2a8d289bfd98a51ae0fe83c47628
suke358 <123350462+suke358@users.noreply.github.com>
2026-01-08 16:17:50 +0900

    データ保存改善

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'meal-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/script.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
